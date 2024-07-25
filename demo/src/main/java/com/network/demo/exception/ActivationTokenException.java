8b750e0d7550fcd3aa92892c06be967dcbe6f305
Mohamed <mohame@gmail.com>
2024-07-25 23:27:36 +0100

    handling d'exeception

---
+++ demo/src/main/java/com/network/demo/exception/ActivationTokenException.java
@@ -0,0 +1,9 @@
+package com.network.demo.exception;
+
+public class ActivationTokenException extends RuntimeException {
+    public ActivationTokenException(String message) {
+        super(message);
+    }
+
+
+}
---
+++ demo/src/main/java/com/network/demo/exception/OperationNotPermittedException.java
@@ -0,0 +1,12 @@
+package com.network.demo.exception;
+
+public class OperationNotPermittedException extends RuntimeException {
+
+    public OperationNotPermittedException() {
+    }
+
+    public OperationNotPermittedException(String message) {
+        super(message);
+    }
+
+}
